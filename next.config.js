5121dfc22f94740865fa1af4c84409095a116a34
percho <ivanhuay@gmail.com>
2019-03-06 16:59:45 -0300

    try solution

--- next.config.js
+++ next.config.js
@@ -1,3 +1,22 @@
 const withCSS = require('@zeit/next-css')
 const withSASS = require('@zeit/next-sass')
-module.exports = withCSS(withSASS());
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = withCSS(withSASS({
+  webpack(config, options){
+    const { plugins} = config;
+    const {isServer} = options;
+    if(!isServer){
+      delete config.optimization.splitChunks.cacheGroups.styles;
+    }
+    console.log(Object.keys(config));
+    plugins.forEach((plugin, index) => {
+      if (plugin.constructor.name === 'MiniCssExtractPlugin') {
+            plugins[index] = new MiniCssExtractPlugin({
+                filename: 'static/css/[contenthash].css',
+                chunkFilename: 'static/css/[contenthash].css',
+            });
+        }
+    });
+    return config;
+  }
+}));
